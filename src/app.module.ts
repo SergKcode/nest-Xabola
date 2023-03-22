a7b6468aac9fc3658dd56147b23bc28fcb9a7040
unknown <sergio-caballero_student2021@wilder.school>
2023-03-22 22:45:08 +0100

    creating docker container and volume postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MaterialsModule } from './materials/materials.module';
+import { ModulesModule } from './modules/modules.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, MaterialsModule, ModulesModule],
   controllers: [AppController],
   providers: [AppService],
 })
